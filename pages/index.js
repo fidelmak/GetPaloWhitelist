1aacff2889f65157b6f7bb09b0e22ce84eb57630
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-12-25 03:52:19 +0100

    new file:   constants/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState, useRef, useEffect } from 'react'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Web3Modal  from 'web3modal' 
-import { providers, Contract } from 'ethers'
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 
 
@@ -14,9 +12,11 @@ export default function Home() {
   const b = () =>{
     return console.log('it worked ')
   }
-const[walletConnected, setWalletConnected]= useState(false)
+const[walletConnected, setWalletConnected]= useState(false);
+const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+const [loading, setLoading] = useState(false);
 const [numOfWhitelisted, setNumOfWhitelisted] = useState(0)
-const Web3ModalRef = useRef();
+const web3ModalRef = useRef();
 
 
 const getProviderOrSigner = async (needSigner = false) => {
@@ -39,6 +39,64 @@ const getProviderOrSigner = async (needSigner = false) => {
   return web3Provider;
 };
 
+// add addAddressToWhitelist: this adds the current address to the whitelist 
+const addNumberToWhitelist = async () =>{
+  try {
+    const signer = await getProviderOrSigner(true);
+    const whitelistContract = new Contract(
+      WHITELIST_CONTRACT_ADDRESS,
+      abi,
+      signer
+    );
+      const tx = await whitelistContract.addNumberToWhitelist()
+      setLoading(true);
+      await tx.wait();
+      setLoading(false)
+
+      await getNumberOfWhitelisted()
+      setJoinedWhitelist(true);
+
+     } catch (err){
+    console.log(err);
+  }
+};
+const getNumberOfWhitelisted = async () => {
+  try{
+    const provider = await getProviderOrSigner();
+    const whitelistContract = new Contract(
+      WHITELIST_CONTRACT_ADDRESS,
+      abi,
+      provider
+    );
+
+    const _numberOfWhitelisted = await whitelistContract.numAddressesWhitelisted();
+    setNumOfWhitelisted(_numberOfWhitelisted);
+
+
+  } catch(err){
+    console.log(err);
+  }
+}
+const checkIfAddressInWhitelist = async () =>{
+  try{
+    const signer = await getProviderOrSigner(true);
+    const whitelistContract = new Contract(
+      WHITELIST_CONTRACT_ADDRESS,
+      abi,
+      signer
+    )
+    const address = await signer.getAddress();
+
+    const _joinedWhitelist = await whitelistContract.whitelistedAddress(
+      address
+    );
+    setJoinedWhitelist(_joinedWhitelist);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 
 
@@ -56,12 +114,44 @@ const connectWallet = async () => {
   }
 };
 
+
+const renderButton = () => {
+  if(walletConnected){
+    if(joinedWhitelist){
+      return (
+        <div className={styles.description}>
+          Thanks for Joining the Whitelist
+        </div>
+      );
+    } else if (loading) {
+      return <button className={styles.button}>Loading...</button>
+    } else{
+      return (
+        <button onClick={addNumberToWhitelist} className={styles.button}>
+          Join the Whitelist
+        </button>
+      )
+    }
+  } else {
+    return (
+      <button onClick={connectWallet} className={styles.button}>
+        Connect your wallet
+      </button>
+    )
+  }
+}
+
+
+
+
+
+
 useEffect(() => {
   if(!walletConnected){
-      Web3ModalRef.current = new Web3Modal({
+      web3ModalRef.current = new Web3Modal({
       network: "goerli",
-      providerOption: {},
-      disabledInjectedProvide:false,
+      providerOptions: {},
+      disabledInjectedProvider:false,
     });
     connectWallet();
   }
@@ -74,6 +164,7 @@ useEffect(() => {
       <Head>
           <title>Whitelist dApp</title>
           <meta name="description" content='whitelist-dApp' />
+          <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         <div className={styles.main}>
@@ -83,7 +174,8 @@ useEffect(() => {
           <div className={styles.description}>
             {numOfWhitelisted} devs already Joined the whitelist
           </div>
-          <button className={styles.button} onClick={b }>Join the whitelist</button>
+          {/* <button className={styles.button} onClick={b}>Join the whitelist</button> */}
+          {renderButton()}
           </h1>
           <div>
             <img className={styles.image} src='./crypto-devs.svg'/>
